70fe58b69b28920cbc5e85504455827e66622a29
AliEn707 <1alien707@qip.ru>
2015-07-28 15:06:18 +0400

    build hash for db from vcard hash

--- lib/vines/other/hash_to_vcard.rb
+++ lib/vines/other/hash_to_vcard.rb
@@ -27,7 +27,11 @@ class Hash
 	end
 	
 	def build_hash(o=nil)
-		#TODO: add, see comment
+		if !(o) then 
+			MAPPING.build_hash(self[:vCard]) 
+		else 
+			self.inject({}){|c,(k,v)| c.merge(v.build_hash(o[k]))}
+		end
 	end
 	
 	def to_vcard
@@ -39,14 +43,21 @@ class Array
 	def build_vcard(o=nil)
 		return self.map{|e| o[e]}.join(" ")
 	end
+
+	def build_hash(o=nil)
+		return {}
+	end
 end
 
 class Object
 	def build_vcard(o=nil)
 		return o[self]
 	end
+	
+	def build_hash(o=nil)
+		return {self => o}
+	end	
 end
-
 
 =begin
 {:vCard=>{:prodid=>"-//HandGen//NONSGML vGen v1.0//EN", :version=>"2.0", :FN=>"1", :N=>{:FAMILY=>"2", :GIVEN=>"3"}, :NICKNAME=>"4", :URL=>"5", :ADR=>{:STREET=>"6", :EXTADD=>"7", :LOCALITY=>"8", :REGION=>"9", :PCODE=>"10", :CTRY=>"11"}, :TEL=>{:NUMBER=>"12"}, :EMAIL=>{:USERID=>"13"}, :ORG=>{:ORGNAME=>"14", :ORGUNIT=>"15"}, :TITLE=>"16", :ROLE=>"17", :BDAY=>"18", :DESC=>"123131231313"}}
